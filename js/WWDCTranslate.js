9eb1fc6b7744e7d56579a96ceb1415500a07434e
finnn1 <tgss3535@gmail.com>
2022-06-10 08:12:15 +0900

    feat: 문단을 문장 단위로 나누는 기능 구현

--- js/WWDCTranslate.js
+++ js/WWDCTranslate.js
@@ -30,6 +30,58 @@ window.onload = function () {
   }
 
 
+
+
+
+  // while (document.querySelector('.tab[data-supplement-id="transcript"]').classList.contains('active') == false) {
+  // Transcript 버튼 클릭하기
+  // const transcriptButton = document.querySelector('[data-supplement-id="transcript"]')
+  // transcriptButton.click()
+
+
+  // ### 문자열을 문장 단위로 합치기 ###
+
+  // 모든 p를 얻어옴 (시간과 함께 나누어져 있는 문장들)
+  var paragraphs = document.querySelector('.supplement.transcript[data-supplement-id="transcript"]').querySelectorAll('p')
+
+  var seperatedSentences
+  var sentencesList = []
+
+  // p 개수 만큼 반복
+  for (pIndex = 0; pIndex < paragraphs.length; pIndex++) {
+    seperatedSentences = paragraphs[pIndex].querySelectorAll('.sentence')
+
+    // 문장 변수
+    var word = ""
+    var currentTime = 0
+    for (sIndex = 0; sIndex < seperatedSentences.length; sIndex++) {
+      var sentenceText = seperatedSentences[sIndex].textContent.trim()
+
+      // word가 비어있다면 -> 첫번째 문장의 시간을 저장해놓기
+      if (word == "") {
+        currentTime = seperatedSentences[sIndex].href.split('=').pop()
+      }
+
+      // .으로 끝난다면
+      var dotIndex = sentenceText.indexOf('.')
+      var sentenceLength = sentenceText.length
+
+      if (dotIndex == (sentenceLength - 1) && dotIndex > 0) {
+        word += " " + sentenceText
+        sentencesList.push([currentTime, word])
+        word = ""
+      } else {
+        word += " " + sentenceText
+      }
+    }
+  }
+  // break
+  // }
+
+
+
+
+
   // ### 비디오가 재생되면 지속적으로 업데이트 ###
   // console.log(aud.currentTime)
   aud.ontimeupdate = function () {
@@ -37,16 +89,11 @@ window.onload = function () {
     // 원본 영문 자막 가져오기
     const transcript = Array.from(document.querySelector('.supplement.transcript[data-supplement-id="transcript"]').querySelectorAll('p'))
 
-    // Transcript 버튼 클릭하기
-    const transcriptButton = document.querySelector('[data-supplement-id="transcript"]')
-    transcriptButton.click()
 
     // 현재 시간과 가장 가까운 p 찾기
-    var currentParagraphTime = 0
-    var paragraphTimes = transcript.map((number, index) => {
-      var paragraphTime = number.querySelector('.sentence').href.split('=').pop()
-      return [index, paragraphTime - (aud.currentTime - 0.05)]
-    });
+    var paragraphTimes = sentencesList.map((number, index) => {
+      return [index, number[0] - (aud.currentTime - 0.05)]
+    })
 
     // 음수는 버리기
     var filteredTimes = paragraphTimes.filter(number => number[1] >= 0)
@@ -58,22 +105,20 @@ window.onload = function () {
     }
 
     // 영어 자막을 시간에 맞게 설정하기
-    if (document.getElementById('TranslateDivEng').textContent != transcript[earlierIndex].textContent) {
-      document.getElementById('TranslateDivEng').textContent = transcript[earlierIndex].textContent
+    if (document.getElementById('TranslateDivEng').textContent != sentencesList[earlierIndex][1]) {
+      document.getElementById('TranslateDivEng').textContent = sentencesList[earlierIndex][1]
       document.getElementById('TranslateDiv').textContent = document.getElementById('TranslateDivEng').textContent
-      console.log(transcript[earlierIndex].textContent)
+      console.log(sentencesList[earlierIndex][1])
     }
 
 
 
 
-  
+
     // ### 한국어로 번역 ###
     if (document.getElementById('TranslateDiv').innerText == document.getElementById('TranslateDivEng').innerText) {
-      console.log("번역 실행됨!!!!!!!!!!!!")
-
       let transText = document.getElementById('TranslateDivEng').textContent
-  
+
       // XMLHttpRequest 파싱
       var xhr = new XMLHttpRequest();
       xhr.open("POST", "https://translation.googleapis.com/language/translate/v2?key=" + apiKey, true);
@@ -83,18 +128,18 @@ window.onload = function () {
         target: "ko",
         source: "en"
       }));
-  
+
       xhr.onreadystatechange = function () {
         if (this.readyState != 4) return;
         if (this.status == 200) {
-          
+
           // 데이터 받아옴
           receivedData = JSON.parse(this.responseText);
         }
 
         // 데이터가 받아온 다음에 실행되는 곳 (비동기식)
         let translatedText = receivedData.data.translations[0].translatedText
-        console.log(translatedText)
+        console.log("번역됨: " + translatedText)
 
         // 번역한 문장 업데이트
         document.getElementById('TranslateDiv').innerText = translatedText
